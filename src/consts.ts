de7cc07de4510ff04076ac2407ae1079c3b4016c
Deer ♡ <61318829+DeerUwU@users.noreply.github.com>
2025-07-10 22:48:04 +0200

    first :3

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Offline\'s Journal';
+export const SITE_DESCRIPTION = 'Thoughts and ideas written for no one in particular.';
