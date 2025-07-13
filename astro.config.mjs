f49d6129ccb11fca1e4b08c058e7ea95d3de2a93
Deer ♡ <61318829+DeerUwU@users.noreply.github.com>
2025-07-13 15:52:27 +0200

    better font loading & astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://offline-blog.neocities.org',
 	integrations: [mdx(), sitemap()],
+
+	trailingSlash: "ignore", //Set the route matching behavior for trailing slashes in the dev server and on-demand rendered pages.
+	output: 'static', //Prerender all your pages by default, outputting a completely static site if none of your pages opt out of prerendering.
+	compressHTML: false,
+
+	experimental: {
+        fonts: [{
+            provider: fontProviders.google(),
+            name: "DM Mono",
+            cssVariable: "--font-DM-Mono"
+        },
+		{
+            provider: fontProviders.google(),
+            name: "Libre Barcode 39",
+            cssVariable: "--font-Barcode-39"
+        }]
+    }
 });
