0eb6064fd147f033bd0392f63277e7e091e75fa7
Deer ♡ <61318829+DeerUwU@users.noreply.github.com>
2025-07-18 05:25:14 +0200

    tweaked particle configs

--- public/particles/loadparticles.js
+++ public/particles/loadparticles.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
-	particlesJS.load("particles-js", "../../particles/particles.json", function(){
+
+  var isMobile = navigator.userAgent.toLowerCase().match(/mobile/i);
+  console.log(`ismobile = ${isMobile}`);
+
+
+  if (isMobile) {
+    particlesJS.load("particles-js", "../../particles/particles_mobile.json", function(){
+      console.log("particles_mobile.json loaded")
+    });
+  } else {
+    particlesJS.load("particles-js", "../../particles/particles.json", function(){
       console.log("particles.json loaded")
     });
+  }
+
 });
